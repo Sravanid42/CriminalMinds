da1fbd6cc1de3b6a82cb8fa586d3dcb4c138d0ff
Catherine <45477636+CatherineGu16@users.noreply.github.com>
2020-04-13 11:00:34 -0700

    Initial sharing of project

---
+++ start.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class start here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class start
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class start
+     */
+    public start()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
