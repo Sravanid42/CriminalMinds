2aee17becd504a8dba619c17039d1d6b9ebf3ffe
Catherine <45477636+CatherineGu16@users.noreply.github.com>
2020-04-21 23:54:14 -0700

    added credentials for AWS not sure how it works yet

--- start.java
+++ start.java
@@ -7,9 +7,24 @@
  */
 public class start
 {
+	/*
     public static void main (String[] args)
     {
-    	
+    	ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+        try {
+            credentialsProvider.getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (/Users/CatherineGu/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        dynamoDB = AmazonDynamoDBClientBuilder.standard()
+            .withCredentials(credentialsProvider)
+            .withRegion("us-west-2")
+            .build();
+
     }
-    
+    */
 }
