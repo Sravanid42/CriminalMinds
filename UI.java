9170e60652e2863442069e5d552273ae0bb5f1bc
Catherine <45477636+CatherineGu16@users.noreply.github.com>
2020-04-30 15:59:52 -0700

    Started UI

---
+++ UI.java
@@ -0,0 +1,51 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import javax.swing.JScrollPane;
+
+public class UI extends JFrame {
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UI frame = new UI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	public UI() {
+		getContentPane().setBackground(new Color(135, 206, 250));
+		getContentPane().setLayout(null);
+		
+		
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+
+
+}
