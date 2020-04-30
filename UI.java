5e9d022f0f839f516dd8aec8846f98517e0f8525
Catherine <45477636+CatherineGu16@users.noreply.github.com>
2020-04-30 16:11:52 -0700

    Put buttons in UI

--- UI.java
+++ UI.java
@@ -15,6 +15,8 @@ import javax.swing.JButton;
 import javax.swing.JTextPane;
 import java.awt.Font;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
 
 public class UI extends JFrame {
 	
@@ -37,6 +39,63 @@ public class UI extends JFrame {
 		getContentPane().setBackground(new Color(135, 206, 250));
 		getContentPane().setLayout(null);
 		
+		JLabel lblCriminalDatabase = new JLabel("Criminal Database");
+		lblCriminalDatabase.setFont(new Font("Rockwell", Font.PLAIN, 18));
+		lblCriminalDatabase.setBounds(329, 0, 164, 37);
+		getContentPane().add(lblCriminalDatabase);
+		
+		textField = new JTextField();
+		textField.setBounds(129, 33, 565, 29);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnDisplayAll = new JButton("Display all");
+		btnDisplayAll.setFont(new Font("Rockwell", Font.PLAIN, 14));
+		btnDisplayAll.setBounds(15, 36, 104, 26);
+		getContentPane().add(btnDisplayAll);
+		
+		JButton btnSearch = new JButton("Search");
+		btnSearch.setFont(new Font("Rockwell", Font.PLAIN, 14));
+		btnSearch.setBounds(704, 36, 104, 26);
+		getContentPane().add(btnSearch);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(10, 114, 798, 390);
+		getContentPane().add(textArea);
+		
+		JButton btnSortByAge = new JButton("Sort by Age");
+		btnSortByAge.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnSortByAge.setFont(new Font("Rockwell", Font.PLAIN, 14));
+		btnSortByAge.setBounds(339, 83, 138, 21);
+		getContentPane().add(btnSortByAge);
+		
+		JButton btnSortByState = new JButton("Sort by State");
+		btnSortByState.setFont(new Font("Rockwell", Font.PLAIN, 14));
+		btnSortByState.setBounds(501, 83, 138, 21);
+		getContentPane().add(btnSortByState);
+		
+		JButton btnLastName = new JButton("Last A-Z");
+		btnLastName.setFont(new Font("Rockwell", Font.PLAIN, 14));
+		btnLastName.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnLastName.setBounds(15, 83, 138, 21);
+		getContentPane().add(btnLastName);
+		
+		JButton btnSortByDate = new JButton("Sort by Date");
+		btnSortByDate.setFont(new Font("Rockwell", Font.PLAIN, 14));
+		btnSortByDate.setBounds(670, 83, 138, 21);
+		getContentPane().add(btnSortByDate);
+		
+		JButton btnFirstName = new JButton("First A-Z");
+		btnFirstName.setFont(new Font("Rockwell", Font.PLAIN, 14));
+		btnFirstName.setBounds(175, 83, 138, 21);
+		getContentPane().add(btnFirstName);
+		
 		
 	}
 
@@ -44,8 +103,5 @@ public class UI extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
-
-
-
+	private JTextField textField;
 }
